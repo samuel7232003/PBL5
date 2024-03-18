418ab8e4e5310921b7f3575387860231755e907f
hi-imchuy <chauhoanghuy562003@gmail.com>
2024-03-18 23:25:23 +0700

    Huy

--- LearnEngApp/src/main/java/com/example/learnengapp/controller/LoginController.java
+++ LearnEngApp/src/main/java/com/example/learnengapp/controller/LoginController.java
@@ -1,11 +1,11 @@
 package com.example.learnengapp.controller;
 
 import com.example.learnengapp.index;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,7 +14,7 @@ public class LoginController {
     @FXML
     public TextField textFieldFocus;
 
-    public void login(ActionEvent event) throws IOException {
+    public void login(MouseEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(index.class.getResource("cameraLayout.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         Stage stage = new Stage();
