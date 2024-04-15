bce03af58b4abd4540b073be246f0037fcb35c4a
hi-imchuy <chauhoanghuy562003@gmail.com>
2024-04-15 15:22:43 +0700

    update

--- LearnEngApp/src/main/java/com/example/learnengapp/controller/ServerDataController.java
+++ LearnEngApp/src/main/java/com/example/learnengapp/controller/ServerDataController.java
@@ -3,6 +3,7 @@ package com.example.learnengapp.controller;
 import com.example.learnengapp.Socket.SocketController;
 import com.example.learnengapp.model.ServerData;
 import com.example.learnengapp.model.Vocab;
+import javafx.application.Platform;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -65,8 +66,8 @@ public class ServerDataController {
             }
             if(n == idWordList.size()) break;
         }
-        getCameraLayoutController().reload();
     }
+
 
     public static ArrayList<Vocab> getVocabToShow() {
         return vocabToShow;
