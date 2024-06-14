f1ac741fd5e81cd63fc70c624d75e1ff204697a1
hi-imchuy <chauhoanghuy562003@gmail.com>
2024-06-14 18:43:08 +0700

    update camera layout

--- LearnEngApp/src/main/java/com/example/learnengapp/controller/CameraLayoutController.java
+++ LearnEngApp/src/main/java/com/example/learnengapp/controller/CameraLayoutController.java
@@ -12,6 +12,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -35,6 +36,8 @@ import static com.example.learnengapp.controller.ServerDataController.setSavePag
 public class CameraLayoutController implements Initializable {
     @FXML
     public HBox listVocab;
+    @FXML
+    public Pane detailsPane;
     @FXML
     private Label test;
     @FXML
@@ -69,9 +72,9 @@ public class CameraLayoutController implements Initializable {
                 pane.setId(vocab.getIdVocab());
                 pane.setStyle("-fx-background-color: #d9d9d9; -fx-background-radius: 8;");
 
-                pane.setPrefHeight(90);
+                pane.setPrefHeight(75);
                 pane.setPrefWidth(210);
-                pane.setMinHeight(90);
+                pane.setMinHeight(75);
                 pane.setMinWidth(210);
 
                 Label word = new Label(vocab.getWord());
@@ -83,20 +86,20 @@ public class CameraLayoutController implements Initializable {
                 Label mean = new Label(vocab.getMean());
                 pane.getChildren().add(mean);
                 mean.setLayoutX(13);
-                mean.setLayoutY(60);
+                mean.setLayoutY(45);
                 mean.setStyle("-fx-font-size: 16; -fx-font-weight: bold;");
-
-                Label phonetic = new Label(vocab.getPhonetic());
-                pane.getChildren().add(phonetic);
-                phonetic.setLayoutX(13);
-                phonetic.setLayoutY(40);
-                phonetic.setStyle("-fx-font-size: 14;");
+//
+//                Label phonetic = new Label(vocab.getPhonetic());
+//                pane.getChildren().add(phonetic);
+//                phonetic.setLayoutX(13);
+//                phonetic.setLayoutY(40);
+//                phonetic.setStyle("-fx-font-size: 14;");
 
                 ImageView speaker = new ImageView();
                 pane.getChildren().add(speaker);
                 speaker.setFitWidth(34);
                 speaker.setFitHeight(34);
-                speaker.setLayoutX(125);
+                speaker.setLayoutX(170);
                 speaker.setLayoutY(35);
                 Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/learnengapp/icon/speaker.png")));
                 speaker.setImage(image);
@@ -112,6 +115,76 @@ public class CameraLayoutController implements Initializable {
                         setSavePage(1);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
+                    }
+                });
+
+                pane.setOnMouseEntered(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent mouseEvent) {
+                        Pane pane = new Pane();
+                        detailsPane.getChildren().add(pane);
+                        pane.setLayoutX(20);
+                        pane.setLayoutY(20);
+                        pane.setPrefHeight(105);
+                        pane.setPrefWidth(328);
+                        pane.setStyle("-fx-border-color: black; -fx-border-radius: 8");
+
+                        Label word = new Label(vocab.getWord());
+                        pane.getChildren().add(word);
+                        word.setLayoutX(14);
+                        word.setLayoutY(7);
+                        word.setPrefSize(286, 31);
+                        word.setStyle("-fx-font-weight: bold; -fx-font-size: 18");
+
+                        Label mean = new Label(vocab.getMean());
+                        pane.getChildren().add(mean);
+                        mean.setLayoutX(14);
+                        mean.setLayoutY(73);
+                        mean.setPrefSize(286, 18);
+                        mean.setStyle("-fx-font-family: Inter; -fx-font-size: 14");
+
+                        Label phonetic = new Label(vocab.getPhonetic());
+                        pane.getChildren().add(phonetic);
+                        phonetic.setLayoutX(14);
+                        phonetic.setLayoutY(40);
+                        phonetic.setPrefSize(286, 25);
+                        phonetic.setStyle("-fx-font-size: 16");
+
+                        Label cd = new Label("Cách dùng");
+                        detailsPane.getChildren().add(cd);
+                        cd.setLayoutX(20);
+                        cd.setLayoutY(134);
+                        cd.setStyle("-fx-font-weight: bold; -fx-font-size: 14");
+
+                        Label example = new Label(vocab.getExample());
+                        detailsPane.getChildren().add(example);
+                        example.setLayoutX(100);
+                        example.setLayoutY(131);
+                        example.setPrefSize(346, 46);
+
+                        Label imageMH = new Label("Hình ảnh minh họa");
+                        detailsPane.getChildren().add(imageMH);
+                        imageMH.setLayoutX(20);
+                        imageMH.setLayoutY(190);
+                        imageMH.setStyle("-fx-font-size: 14");
+
+                        ImageView imageView = new ImageView();
+                        detailsPane.getChildren().add(imageView);
+                        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/learnengapp/image/" + vocab.getIdVocab() + ".jpg")));
+                        imageView.setImage(image);
+                        imageView.setFitHeight(126);
+                        imageView.setFitWidth(179);
+                        imageView.setLayoutX(194);
+                        imageView.setLayoutY(181);
+                        imageView.setPickOnBounds(true);
+                        imageView.setPreserveRatio(true);
+                    }
+                });
+
+                pane.setOnMouseExited(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent mouseEvent) {
+                        detailsPane.getChildren().clear();
                     }
                 });
 
